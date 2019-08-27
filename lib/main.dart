b48ee0647e977509604c0d152cd025a52e6201fb
davidbanhos <david.banhos@gmail.com>
2019-08-27 19:53:48 +1000

    Final part1.

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
-    final wordPair = WordPair.random();
-
     return MaterialApp(
       title: 'Welcome to Flutter',
       home: Scaffold(
@@ -20,9 +18,7 @@ class MyApp extends StatelessWidget {
           title: Text('Welcome to Flutter'),
         ),
         body: Center(
-//          child: Text('Formiga preta!'),
-//            child: Text(wordPair.asPascalCase),
-            child: RandomWords(),
+          child: RandomWords(),
         ),
       ),
     );
@@ -30,13 +26,42 @@ class MyApp extends StatelessWidget {
 }
 
 class RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
 
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-    print(wordPair);
-    return Text(wordPair.asPascalCase);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Startup Name Generator'),
+      ),
+      body: _buildSuggestions(),
+    );
   }
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: (context, i) {
+          if (i.isOdd) return Divider();
+
+          final index = i ~/ 2; /*3*/
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
+  }
+
 }
 
 class RandomWords extends StatefulWidget {
